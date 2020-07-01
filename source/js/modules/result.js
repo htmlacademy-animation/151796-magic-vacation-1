0b6eae3eae4fcd9a3ba9ebb56cb35e578178d335
shelby <shelby@gmail.com>
2020-07-01 19:09:50 +0300

    Made defeat title animation

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -1,24 +1,52 @@
 const animateTitle = (screenId) => {
-  const titleSvg = document.querySelector(`#${screenId} .game__victory-title`);
+  const titleSvg = document.querySelector(`#${screenId} .result__svg-title`);
   if (titleSvg) {
+    const type = titleSvg.getAttribute(`data-type`);
+    let letterDelay = 0;
+
     const paths = titleSvg.querySelectorAll(`path`);
     paths.forEach((path) => {
       const len = path.getTotalLength();
-      const animate = document.createElementNS(`http://www.w3.org/2000/svg`, `animate`);
+      const animateStrokeDashArray = document.createElementNS(`http://www.w3.org/2000/svg`, `animate`);
 
       path.setAttribute(`stroke-dasharray`, `0 ` + Math.ceil(len / 3));
-      animate.setAttribute(`attributeName`, `stroke-dasharray`);
-      animate.setAttribute(`from`, `0 ${Math.ceil(len / 3)}`);
-      animate.setAttribute(`to`, `${Math.ceil(len / 3)} 0`);
-      animate.setAttribute(`dur`, `0.6s`);
-      animate.setAttribute(`begin`, `start`);
-      animate.setAttribute(`fill`, `freeze`);
+      animateStrokeDashArray.setAttribute(`attributeName`, `stroke-dasharray`);
+      animateStrokeDashArray.setAttribute(`from`, `0 ${Math.ceil(len / 3)}`);
+      animateStrokeDashArray.setAttribute(`to`, `${Math.ceil(len / 3)} 0`);
+      animateStrokeDashArray.setAttribute(`fill`, `freeze`);
+      if (type === `defeat`) {
+        animateStrokeDashArray.setAttribute(`begin`, `start + ${letterDelay}ms`);
+        animateStrokeDashArray.setAttribute(`dur`, `0.5s`);
+      } else {
+        animateStrokeDashArray.setAttribute(`begin`, `start`);
+        animateStrokeDashArray.setAttribute(`dur`, `0.6s`);
+      }
 
-      path.appendChild(animate);
+      path.appendChild(animateStrokeDashArray);
+
+      if (type === `defeat`) {
+        const animateTransition = document.createElementNS(`http://www.w3.org/2000/svg`, `animateTransform`);
+
+        animateTransition.setAttribute(`attributeName`, `transform`);
+        animateTransition.setAttribute(`type`, `translate`);
+        animateTransition.setAttribute(`values`, `0 -50; 0 60; 0 45; 0 50`);
+        animateTransition.setAttribute(`keyTimes`, `0; 0.85; 0.93; 1`);
+        animateTransition.setAttribute(`calcMode`, `spline`);
+        animateTransition.setAttribute(`keySplines`, `0.11 0 0.92 0.41; 0.25 1 0.5 1; 0.37 0 0.63 1`);
+        animateTransition.setAttribute(`dur`, `0.8s`);
+        animateTransition.setAttribute(`additive`, `sum`);
+        animateTransition.setAttribute(`fill`, `freeze`);
+        animateTransition.setAttribute(`begin`, `start + ${letterDelay}ms`);
+
+        letterDelay += 40;
+
+        path.appendChild(animateTransition);
+      }
+
       path.dispatchEvent(new Event(`start`));
     });
   }
-  titleSvg.classList.add(`game__victory-title--displayed`);
+  titleSvg.classList.add(`result__svg-title--displayed`);
 };
 
 export default () => {
